17d16bd2a3f5f3d2433fc0a2055fff34f20480eb
Yahvi <yashvisingh2912@gmail.com>
2026-05-31 16:53:15 +0530

    updated configuration

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     SECRET_KEY:str
     ALGORITHM:str="HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES:int=30
+    REDIS_URL:str="redis://localhost:6379"
 
     class Config:
         env_file=".env"
